0cf35bdd4442d2dd19fd9a1d022ac150d36d0538
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-02-03 16:56:12 +0100

    TP3 start

--- ex1.py
+++ ex1.py
@@ -5,55 +5,51 @@ import numpy as np
 import matplotlib.pyplot as mpl
 
 
-def calc_errors(coef):
-    y = coef.item(0)*temps+coef.item(1)
-    mpl.plot(y, temps, label= u'\u0398\u2081: '+ str(coef.item(0)) + u'\t\u0398\u2082: ' + str(coef.item(1)))
-    
-    Jabs = sum(abs(position-y))/N
-    print("Jabs: \t\t" + str(Jabs))
-
-    # Jl1_test = (sum((position-y)**2)**(0.5))/N
-    # print("Jl1_test: \t" + str(Jl1_test))
-
-    # Jl2_test = sum((position-y)**2)/N
-    # print("Jl2_test: \t" + str(Jl2_test))
-
+def jl2(coef):
     part_1 = (position_matrix - np.dot(temps_matrix_transpose, coef)).transpose()
     part_2 =  position_matrix - np.dot(temps_matrix_transpose, coef)
-
-    JL1 = np.dot(part_1,part_2).item(0)**0.5/N
-    print("JL1: \t\t" + str(JL1))
-
-    Jl2 = np.dot(part_1,part_2).item(0)/N
-    print("JL2: \t\t" + str(Jl2))
-
-    Jlinf = max(abs(position-y))
-    print("Jlinf: \t\t" + str(Jlinf))
+    return np.dot(part_1,part_2).item(0)/N
 
 coef = np.matrix('2;3') # O
 
 position = np.loadtxt("p.txt")
 temps = np.loadtxt("t.txt")
-mpl.plot(position, temps, ".")
 
 N = position.size
 
-position_matrix = np.matrix(position).transpose() # y
+position_matrix = np.matrix(position).transpose() # Y
 
 one_array = [1]*temps.size
 temps_matrix = np.matrix(np.vstack((temps, one_array))) # X
 temps_matrix_transpose = temps_matrix.transpose()
 
-calc_errors(coef)
+A_const = 100000.
+B_const = 30.
+C_const = 400.
+min_diff = 0.00000001
+t = 100.
 
-teta_part_one = np.dot(temps_matrix, temps_matrix_transpose)**(-1)
-teta_part_two = np.dot(temps_matrix, position_matrix)
+def alpha_t(t):
+    return A_const/(B_const+C_const*t)
 
-teta = np.dot(teta_part_one, teta_part_two)
+def gradient_descent(teta_old, t):
+    step_1 = np.dot(temps_matrix_transpose, teta_old)
+    step_2 = np.dot(temps_matrix, (position_matrix - step_1))
+    teta_new = teta_old + alpha_t(t) * step_2 / N
+    return teta_new
 
-print("Teta: " + str(teta))
+teta_old = coef
+teta_new = gradient_descent(teta_old, t)
 
-calc_errors(teta)
+list_jl2_teta = []
 
-mpl.legend(bbox_to_anchor=(0, 1), loc=2, borderaxespad=0.)
+while abs(jl2(teta_new) - jl2(teta_old)) > min_diff:
+    teta_old = teta_new
+    t+=1
+    teta_new = gradient_descent(teta_old, t)
+    list_jl2_teta.append(jl2(teta_new))
+
+print(teta_new)
+
+mpl.plot(list_jl2_teta)
 mpl.savefig("graph.jpeg")
